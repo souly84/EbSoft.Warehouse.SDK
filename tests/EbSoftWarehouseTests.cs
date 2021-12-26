6adf085b27cb25dd6b9f817d407679c89e8c0109
Denis Zhukovski <36443640+DenisZhukovsky@users.noreply.github.com>
2021-12-26 14:20:54 +0300

    Storage by barcode (#8)
    * update core package
    
    * implement storage by barcode
    
    * add integration test
    
    * update unit tests
    
    * update unit tests
    
    * fix unit tests coverage
    
    * fix static code analyses issues
    
    Co-authored-by: Denis Zhukovski <dzhukovsky@softek.by>

---
+++ tests/EbSoftWarehouseTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using EbSoft.Warehouse.SDK.UnitTests.Extensions;
+using MediaPrint;
+using Warehouse.Core;
+using Xunit;
+using Assert = EbSoft.Warehouse.SDK.UnitTests.Extensions.Assert;
+
+namespace EbSoft.Warehouse.SDK.UnitTests
+{
+    public class EbSoftWarehouseTests
+    {
+        [Fact(Skip = GlobalTestsParams.AzureDevOpsSkipReason)]
+        public async Task ByBarcodeInWarehouseIntegration()
+        {
+            Assert.NotNull(
+                await new EbSoftCompany(
+                    ConfigurationManager.AppSettings["companyUri"]
+                ).Warehouse
+                 .ByBarcodeAsync("133037620160")
+            );
+        }
+
+        [Fact]
+        public async Task ByBarcodeInWarehouse()
+        {
+            Assert.NotNull(
+                await new EbSoftCompany(
+                    new FakeBackend().ToWebRequest()
+                ).Warehouse
+                 .ByBarcodeAsync("133037620160")
+            );
+        }
+    }
+}
