b77de412ce18aeab5e49a9eae1bafd35249fa8d9
Denis Zhukovski <dzhukovsky@softek.by>
2022-02-02 12:22:43 +0300

    package version update + test coverage improvements

--- tests/EbSoftReceptionGoodTests.cs
+++ tests/EbSoftReceptionGoodTests.cs
@@ -8,26 +8,72 @@ namespace EbSoft.Warehouse.SDK.UnitTests
 {
     public class EbSoftReceptionGoodTests
     {
+        private string _goodAsJson =
+            @"{
+                ""id"":""22"",
+                ""oa_dossier"":""OA843460"",
+                ""article"":""BEKO HCA63640BH"",
+                ""qt"":""5"",
+                ""ean"":[ ""8690842130830"" ],
+                ""qtin"":""5"",
+                ""error_code"":null,
+                ""commentaire"":null,
+                ""itemType"":""electro"",
+                ""qtscanned"":""1""
+            }";
+
         [Fact]
         public async Task InitiallyConfirmedReceptionGood()
         {
             Assert.True(
                 await new EbSoftReceptionGood(
                     1,
-                    JObject.Parse(
-                        @"{
-                            ""id"":""22"",
-                            ""oa_dossier"":""OA843460"",
-                            ""article"":""BEKO HCA63640BH"",
-                            ""qt"":""5"",
-                            ""ean"":[ ""8690842130830"" ],
-                            ""qtin"":""5"",
-                            ""error_code"":null,
-                            ""commentaire"":null,
-                            ""itemType"":""electro"",
-                            ""qtscanned"":""1""}"
-                    )
+                    JObject.Parse(_goodAsJson)
                 ).ConfirmedAsync()
+            );
+        }
+
+        [Fact]
+        public void UnknownGood()
+        {
+            Assert.True(
+                new EbSoftReceptionGood(
+                    1,
+                    "UnknownBarcode"
+                ).IsUnknown
+            );
+        }
+
+        [Fact]
+        public async Task CanBeFoundByArticle()
+        {
+            Assert.True(
+                new EbSoftReceptionGood(
+                    1,
+                    JObject.Parse(_goodAsJson)
+                ).Equals("electro")
+            );
+        }
+
+        [Fact]
+        public async Task CanBeFoundByItemType()
+        {
+            Assert.True(
+                new EbSoftReceptionGood(
+                    1,
+                    JObject.Parse(_goodAsJson)
+                ).Equals("BEKO HCA63640BH")
+            );
+        }
+
+        [Fact]
+        public async Task CanBeFoundById()
+        {
+            Assert.True(
+                new EbSoftReceptionGood(
+                    1,
+                    JObject.Parse(_goodAsJson)
+                ).Equals(22)
             );
         }
     }
