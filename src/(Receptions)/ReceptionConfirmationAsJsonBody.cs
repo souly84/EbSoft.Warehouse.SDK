d55744304fe5c6177e3baa6405d5071fd2a801fe
Denis Zhukovski <dzhukovsky@softek.by>
2022-02-26 11:03:53 +0300

    split extra confirmed item into 2 items

--- src/(Receptions)/ReceptionConfirmationAsJsonBody.cs
+++ src/(Receptions)/ReceptionConfirmationAsJsonBody.cs
@@ -27,11 +27,10 @@ namespace EbSoft.Warehouse.SDK
                 if (confirmation.ConfirmedQuantity > 0)
                 {
                     var goodData = confirmation.Good.ToDictionary();
-                    var id = goodData.Value<string>("Id");
                     array.Add(
                         new JObject(
-                            new JProperty("id", id == "0" ? "" : id), // when id == "0" means unknown good
-                            new JProperty("qty", confirmation.ConfirmedQuantity),
+                            new JProperty("id", Id(confirmation.Good)), 
+                            new JProperty("qty", ConfirmedQty(confirmation)),
                             new JProperty("gtin", goodData.Value<string>("Ean")),
                             new JProperty("error_code", null)
                         )
@@ -43,6 +42,23 @@ namespace EbSoft.Warehouse.SDK
                 new JProperty("cmrId", _receptionId),
                 new JProperty("lines", array)
             ).ToString();
+        }
+
+        private int ConfirmedQty(IGoodConfirmation confirmation)
+        {
+            if (confirmation.Good.IsExtraConfirmed)
+            {
+                return confirmation.ConfirmedQuantity - confirmation.Good.Quantity;
+            }
+            return confirmation.ConfirmedQuantity;
+        }
+
+        private string Id(IReceptionGood receptionGood)
+        {
+            // when id == "0" means unknown good
+            return receptionGood.Id == "0" || receptionGood.IsExtraConfirmed
+                ? string.Empty
+                : receptionGood.Id;
         }
     }
 }
