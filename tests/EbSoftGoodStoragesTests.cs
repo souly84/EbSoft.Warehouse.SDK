92c7aa0007d80f40469d3b5361adfe35f6b72cda
Denis Zhukovski <dzhukovsky@softek.by>
2022-03-15 20:14:58 +0300

    improve test coverage

--- tests/EbSoftGoodStoragesTests.cs
+++ tests/EbSoftGoodStoragesTests.cs
@@ -94,6 +94,32 @@ namespace EbSoft.Warehouse.SDK.UnitTests
         }
 
         [Fact]
+        public async Task GetHasCodeNotZero()
+        {
+            var good = await new EbSoftFakeServer()
+                .Warehouse()
+                .Goods.FirstAsync("4002516315155");
+            var storages = good.Storages;
+
+            Assert.NotEqual(
+                0,
+                storages.GetHashCode()
+            );
+        }
+
+        [Fact]
+        public async Task EqualToTheSame()
+        {
+            var good = await new EbSoftFakeServer()
+                .Warehouse()
+                .Goods.FirstAsync("4002516315155");
+            Assert.Equal(
+                good.Storages,
+                good.Storages
+            );
+        }
+
+        [Fact]
         public async Task DoesNotSupportFilters()
         {
             var good = await new EbSoftFakeServer()
